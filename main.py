af67cc609aee396026f291281d7f2581bbd999b7
Denis-Leo777 <denis25071987@gmail.com>
2025-04-17 14:43:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -188,11 +188,11 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     default_model_name = AVAILABLE_MODELS.get(DEFAULT_MODEL, DEFAULT_MODEL)
     start_message = (
         f"**{default_model_name}** - модель по умолчанию."
-        f"\n Поиск Google/DDG включен, используются улучшенные настройки точности, логики из юмора."
-        f"\n Я также умею читать картинки (с текстом и без) и текстовые файлы."
-        f"\n `/model` — сменить модель,"
-        f"\n `/search_on` / `/search_off` — вкл/выкл поиск,"
-        f"\n `/clear` — очистить историю диалога."
+        f"\nПоиск Google/DDG включен, используются улучшенные настройки точности, логики из юмора."
+        f"\nЯ также умею читать картинки (с текстом и без) и текстовые файлы."
+        f"\n /model — сменить модель,"
+        f"\n /search_on / /search_off — вкл/выкл поиск,"
+        f"\n /clear — очистить историю диалога."
     )
     await update.message.reply_text(start_message, parse_mode='Markdown')
 
